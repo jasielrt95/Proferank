f81daa90c033731da130a7dfe2ec246d4da6d327
Jasiel Rivera <jasielrt95@gmail.com>
2022-12-30 20:04:25 -0400

    fixed base html and static files ( sort off)

--- iupihub/urls.py
+++ iupihub/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('professors/', include(('professors.urls', 'professors'), namespace='professors')),
     path('courses/', include(('courses.urls', 'courses'), namespace='courses')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
