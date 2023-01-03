5371a86b02e6e682eb8d7a61ad8270690d970592
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-02 21:51:00 -0400

    added different types of comments

--- comments/admin.py
+++ comments/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Comment
+from .models import Comment, Confession_Comment, Reply_Comment, Course_Comment
 
-admin.site.register(Comment)
+admin.site.register(Confession_Comment)
+admin.site.register(Reply_Comment)
+admin.site.register(Course_Comment)
+
--- comments/models.py
+++ comments/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Comment(models.Model):
-    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     text = models.TextField(null=False)
 
@@ -15,9 +14,31 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+    class Meta:
+        abstract = True
+
     @property
     def score(self):
         return self.upvotes - self.downvotes
     
+    def __str__(self):
+        return self.text
+
+class Confession_Comment(Comment):
+    confession = models.ForeignKey("confessions.Confession", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Course_Comment(Comment):
+    course = models.ForeignKey("courses.Course", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Reply_Comment(Comment):
+    comment = models.ForeignKey("comments.Confession_Comment", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.text
