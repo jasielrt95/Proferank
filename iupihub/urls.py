50b6649fb88d40ef500405491b3e962f71092da8
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-04 16:09:01 -0400

    made navbar dynamic based on auth

--- iupihub/urls.py
+++ iupihub/urls.py
@@ -19,12 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('home.urls'), name='home'),
     path('admin/', admin.site.urls),
     path('professors/', include(('professors.urls', 'professors'), namespace='professors')),
     path('courses/', include(('courses.urls', 'courses'), namespace='courses')),
-    path('accounts/', include("django.contrib.auth.urls"))
-
+    path('accounts/', include("accounts.urls")),
 ]
 
 if settings.DEBUG:
