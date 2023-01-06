3e7cfd0903e487efb7ca232edcd7a414bcdaa85d
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-05 20:30:18 -0400

    path bug fix

--- iupihub/wsgi.py
+++ iupihub/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+path = "/Users/jasielrivera/Desktop/iupihub/"
+if path not in sys.path:
+    sys.path.append(path)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings")
 
