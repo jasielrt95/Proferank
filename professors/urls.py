400b46957dbe99b173cf0bef787dc39dc48073bb
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-05 20:28:11 -0400

    improved professors views and html

--- professors/urls.py
+++ professors/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 
-from .views import ProfessorListView, ProfessorDetailView
+from .views import ProfessorListView, ProfessorDetailView, ProfessorCreateView
 
 app_name = "professors"
 
 urlpatterns = [
     path("all", ProfessorListView.as_view(), name="all_professors"),
     path("<int:pk>", ProfessorDetailView.as_view(), name="specific_professor"),
+    path("create", ProfessorCreateView.as_view(), name="create_professor"),
+
 ]
--- professors/views.py
+++ professors/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Professor
-
+from courses.models import Course
+from comments.models import Course_Comment
+from .forms import RateProfessorForm, CreateProfessorForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 class ProfessorListView(ListView):
     model = Professor
@@ -16,5 +20,18 @@ class ProfessorDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         profesor = Professor.objects.get(id=self.kwargs["pk"])
+        context["courses"] = Course.objects.filter(professor=profesor)
+        # get the comments from the courses associated with the professor
+        context["comments"] = Course_Comment.objects.filter(
+            course__in=Course.objects.filter(professor=profesor)
+        )
+        context["form"] = RateProfessorForm()
         context["professor"] = profesor
         return context
+
+class ProfessorCreateView(LoginRequiredMixin, CreateView):
+    model = Professor
+    template_name = "create_professor.html"
+    form = CreateProfessorForm
+    fields = ["first_name", "last_name", "college", "faculty"]
+    success_url = reverse_lazy("professors:all_professors")
