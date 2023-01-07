1003d6b2f21714b6d5226f6d9c891e982e7dff8d
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-06 22:28:09 -0400

    changed choices for college & improved admin view

--- professors/admin.py
+++ professors/admin.py
@@ -3,4 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Professor
 
-admin.site.register(Professor)
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "college",
+        "faculty",
+        "average_score",
+        "average_difficulty",
+        "average_grade",
+        "average_pro_student",
+        "average_organized",
+    )
+    list_filter = ("college", "faculty")
+    search_fields = ("first_name", "last_name", "college", "faculty")
+
+admin.site.register(Professor, ProfessorAdmin)
--- professors/models.py
+++ professors/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from courses.models import Course
+from reviews.models import Review
 
 
 class Professor(models.Model):
@@ -19,8 +19,8 @@ class Professor(models.Model):
     FACULTIES = ((Natu, "Naturales"), (Huma, "Humanidades"))
 
     # Colleges
-    UPRRP = "Universidad de Puerto Rico, Rio Piedras"
-    UPRM = "Universidad de Puerto Rico, Mayaguez"
+    UPRRP = "UPRRP"
+    UPRM = "UPRM"
     COLLEGES = ((UPRRP, "UPRRP"), (UPRM, "UPRM"))
 
     # Professor information
@@ -29,18 +29,73 @@ class Professor(models.Model):
     college = models.CharField(choices=COLLEGES, max_length=100)
     faculty = models.CharField(choices=FACULTIES, max_length=30)
 
-    # Getter functions for Professor class
-    def getCollege(self):
-        return self.college
+    @property
+    def average_score(self):
+        courses = Course.objects.filter(professor=self)
+        reviews = Review.objects.filter(course__in=courses)
+        if reviews.count() == 0:
+            return 0
+        return sum([review.score for review in reviews]) / reviews.count()
 
-    def getFaculty(self):
-        return self.faculty
+    @property
+    def average_difficulty(self):
+        courses = Course.objects.filter(professor=self)
+        reviews = Review.objects.filter(course__in=courses)
+        if reviews.count() == 0:
+            return 0
+        return self.num_to_letter(sum([review.difficulty for review in reviews]) / reviews.count())
 
-    def getFirstName(self):
-        return self.first_name
+    @property
+    def average_grade(self):
+        courses = Course.objects.filter(professor=self)
+        reviews = Review.objects.filter(course__in=courses)
+        if reviews.count() == 0:
+            return 0
+        return self.num_to_letter(sum([review.grade for review in reviews]) / reviews.count())
 
-    def getLastName(self):
-        return self.last_name
+    @property
+    def average_pro_student(self):
+        courses = Course.objects.filter(professor=self)
+        reviews = Review.objects.filter(course__in=courses)
+        if reviews.count() == 0:
+            return "No"
+        true = reviews.filter(pro_student=True).count()
+        false = reviews.filter(pro_student=False).count()
+        if true > false:
+            return "Yes"
+        else:
+            return "No"
+
+
+    @property
+    def average_organized(self):
+        courses = Course.objects.filter(professor=self)
+        reviews = Review.objects.filter(course__in=courses)
+        if reviews.count() == 0:
+            return "No"
+        true = reviews.filter(organized=True).count()
+        false = reviews.filter(organized=False).count()
+        if true > false:
+            return "Yes"
+        else:
+            return "No"
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name}"
+
+    def num_to_letter(self, num):
+        if num >= 3.7:
+            return "A"
+        elif num >= 3:
+            return "B"
+        elif num >= 1.7:
+            return "C"
+        elif num >= 1:
+            return "D"
+        else:
+            return "F"
+
+
+            
+    class Meta:
+        ordering = ["last_name", "first_name"]
