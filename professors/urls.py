78bb92662ecaa5c27c4b5ccac299d039d2d0209d
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-06 22:28:37 -0400

    added new views and urls to the professor app

--- professors/urls.py
+++ professors/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("all", ProfessorListView.as_view(), name="all_professors"),
     path("<int:pk>", ProfessorDetailView.as_view(), name="specific_professor"),
     path("create", ProfessorCreateView.as_view(), name="create_professor"),
+    path("<str:college>", ProfessorListView.as_view(), name="college_professors"),
+    path("<str:college>/<str:department>", ProfessorListView.as_view(), name="department_professors"),
 ]
--- professors/views.py
+++ professors/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from .models import Professor
 from courses.models import Course
 from comments.models import Course_Comment
+from reviews.models import Review
 from .forms import RateProfessorForm, CreateProfessorForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -11,6 +12,25 @@ from django.urls import reverse_lazy
 class ProfessorListView(ListView):
     model = Professor
     template_name = "professor_list.html"
+
+    def get_queryset(self):
+        if "college" in self.kwargs:
+            queryset = Professor.objects.filter(college=self.kwargs["college"].upper())
+            if "department" in self.kwargs:
+                queryset = queryset.filter(faculty=self.kwargs["department"].capitalize())
+            return queryset
+        return Professor.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        departments = set()
+        universities = set()
+        for professor in Professor.objects.all():
+            departments.add(professor.faculty)
+            universities.add(professor.college)
+        context["departments"] = departments
+        context["universities"] = universities
+        return context
 
 
 class ProfessorDetailView(DetailView):
@@ -37,3 +57,8 @@ class ProfessorCreateView(LoginRequiredMixin, CreateView):
     form = CreateProfessorForm
     fields = ["first_name", "last_name", "college", "faculty"]
     success_url = reverse_lazy("professors:all_professors")
+
+class ProfessorFilterView(ListView):
+    model = Professor
+    template_name = "professor_list.html"
+
