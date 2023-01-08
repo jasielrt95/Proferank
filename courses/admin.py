ed5eedbac46909357e390f8a34df9ccdca23f6cb
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-08 18:22:36 -0400

    statistics properties

--- courses/admin.py
+++ courses/admin.py
@@ -3,4 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course
 
-admin.site.register(Course)
+
+class courseAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "codification",
+        "professor",
+        "difficulty",
+        "grade",
+        "pro_student",
+        "organized",
+    )
+    list_filter = ("professor",)
+    search_fields = ("name", "codification", "professor")
+
+
+admin.site.register(Course, courseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from reviews.models import Review
 
 
 class Course(models.Model):
@@ -13,6 +14,45 @@ class Course(models.Model):
     # Timestamps
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # Methods
+    @property
+    def grade(self):
+        reviews = Review.objects.filter(course=self)
+        total = 0
+        for review in reviews:
+            total += review.grade_num
+        return total / len(reviews)
+
+    @property
+    def difficulty(self):
+        reviews = Review.objects.filter(course=self)
+        total = 0
+        for review in reviews:
+            total += review.difficulty_num
+        return total / len(reviews)
+
+    @property
+    def pro_student(self):
+        reviews = Review.objects.filter(course=self)
+        total = 0
+        for review in reviews:
+            if review.pro_student:
+                total += 1
+            else:
+                total -= 1
+        return total > 0
+
+    @property
+    def organized(self):
+        reviews = Review.objects.filter(course=self)
+        total = 0
+        for review in reviews:
+            if review.organized:
+                total += 1
+            else:
+                total -= 1
+        return total > 0
 
     def __str__(self):
         return self.codification
