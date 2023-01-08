3ddb2d6bbd4733f1084b256461f95f245795bd55
Jasiel Rivera <jasielrt95@gmail.com>
2023-01-07 23:02:16 -0400

    added Reviews to admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Review
 
-# Register your models here.
 
-admin.site.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "professor":
+            kwargs["queryset"] = db_field.related_model.objects.filter(
+                user=request.user
+            )
+        if db_field.name == "course":
+            kwargs["queryset"] = db_field.related_model.objects.filter(
+                user=request.user
+            )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Review, ReviewAdmin)
