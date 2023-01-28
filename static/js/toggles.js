172e7cea85882b1554d460ff181f6355d30fe6a1
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-01-27 21:15:30 -0400

    new design part 3

---
+++ static/js/toggles.js
@@ -0,0 +1,15 @@
+let comment_toggle = document.getElementById("toggle-comment-form");
+comment_form = document.getElementById("comment-form");
+
+comment_toggle.addEventListener("click", function() {
+    comment_form.classList.toggle("hidden");
+    comment_form.classList.toggle("flex");
+    // toggle the text of the button
+    comment_toggle.innerHTML = comment_toggle.innerHTML == "x" ? "+" : "x";
+});
+
+comment_form.addEventListener("submit", function() {
+    comment_form.classList.toggle("hidden");
+    comment_form.classList.toggle("flex");
+    comment_toggle.innerHTML = comment_toggle.innerHTML == "+" ? "x" : "x";
+});
