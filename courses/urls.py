fda9e447b51fcac0ad04bcf92215284114901cb2
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-01-28 22:00:06 -0400

    enables the creation of new courses

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import CourseListView, CourseDetailView
+from .views import CourseListView, CourseDetailView, CourseCreateView
 
 app_name = "courses"
 
 urlpatterns = [
     path("all", CourseListView.as_view(), name="all_courses"),
     path("<int:pk>", CourseDetailView.as_view(), name="specific_course"),
+    path("professor/<int:pk>/course", CourseCreateView.as_view(), name="create_course_for_professor"),
+    path("create", CourseCreateView.as_view(), name="create_course"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView, View, CreateView
 from .models import Course
 from comments.models import Course_Comment
+from django.urls import reverse
+from professors.models import Professor
+from reviews.models import Review
 
 
 class CourseListView(ListView):
@@ -29,8 +32,35 @@ class CourseDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         course = Course.objects.get(id=self.kwargs["pk"])
+        user_commented = False
+        user_rated  = False
+        if self.request.user.is_authenticated:
+            user_commented = Course_Comment.objects.filter(
+                course=course, user=self.request.user
+            ).exists()
+            user_rated = Review.objects.filter(
+                course=course, user=self.request.user
+            ).exists()
+        context["user_rated"] = user_rated
+        context["user_commented"] = user_commented
         context["course"] = course
         context["professor"] = course.professor
         comments = list(Course_Comment.objects.filter(course=course))
         context["comments"] = comments
         return context
+    
+class CourseCreateView(CreateView):
+    model = Course
+    template_name = "create_course.html"
+    fields = ["name", "codification"]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.professor = Professor.objects.get(id=self.kwargs["pk"])
+        form.save()
+        return redirect("courses:specific_course", pk=form.instance.pk)
+    
+    def form_invalid(self, form):
+        return render(self.request, "create_course.html", {"form": form})
+    
+    
