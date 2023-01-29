1f8a1506be8803d5316ed28ff480fd462aefc24a
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-01-28 21:58:30 -0400

    adds reviewing of courses

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,5 @@ from .views import ReviewFormView
 app_name = "reviews"
 
 urlpatterns = [
-    path("<int:pk>", ReviewFormView.as_view(), name="rate_course"),
+    path("review/<int:pk>", ReviewFormView.as_view(), name="rate_course"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,31 +1,22 @@
-from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from .models import Review
 from courses.models import Course
 from professors.models import Professor
-from .forms import ReviewForm
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 
-class ReviewFormView(FormView):
-    template_name = "rate_professor.html"
-    form_class = ReviewForm
-    success_url = "/"
+class ReviewFormView(CreateView):
+    model = Review
+    template_name = "course_review.html"
+    fields = ["difficulty", "grade", "pro_student", "organized"]
 
     def form_valid(self, form):
-        print("form is valid")
-        user = self.request.user
-        course = Course.objects.get(pk=self.kwargs["pk"])
-        professor = Professor.objects.get(pk=course.professor.pk)
-        form.save_form(user, course, professor)
-        return super().form_valid(form) 
+        form.instance.user = self.request.user
+        form.instance.course = Course.objects.get(id=self.kwargs["pk"])
+        # get the professor from the course
+        form.instance.professor = form.instance.course.professor
+        form.save()
+        return redirect("courses:specific_course", pk=self.kwargs["pk"])
 
-    def form_invalid(self, form):
-        print("form is invalid")
-        return super().form_invalid(form)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["form"] = ReviewForm()
-        return context
+
