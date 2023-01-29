ea3cfaefbe38e9be243b9a4fc779d5af7d367f11
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-01-28 21:59:02 -0400

    enables the creation of comments

---
+++ comments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import CourseCommentCreateView
+
+app_name = "comments"
+
+urlpatterns = [
+    path("course/<int:pk>/comment", CourseCommentCreateView.as_view(), name="course_comment"),
+]
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Course_Comment
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# Create your views here.
+
+class CourseCommentCreateView(CreateView):
+    def post(self, request, *args, **kwargs):
+        course_id = self.kwargs["pk"]
+        comment = Course_Comment.objects.create(
+            course_id=course_id,
+            user=request.user,
+            text=request.POST["comment"],
+        )
+        comment.save()
+        # refresh the page
+        return redirect(reverse("courses:specific_course", args=[course_id]))
+    
+
