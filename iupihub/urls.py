226f662d42f5134958e5aa2a180f17b098393e87
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-02-05 15:18:43 -0400

    include staticfiles_urls

--- iupihub/urls.py
+++ iupihub/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 urlpatterns = [
     path("", include("home.urls"), name="home"),
@@ -32,6 +33,8 @@ urlpatterns = [
     path("comments/", include(("comments.urls", "comments"), namespace="comments")),
     path("social-auth/", include("social_django.urls", namespace="social")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
