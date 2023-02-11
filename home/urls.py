981840310d7762e9e6007a22319bfe7f9aef0fbf
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-02-11 19:19:44 -0400

    adds profile page as home page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import IndexView, AboutView
+from .views import IndexView, AboutView, ProfileView
 
 app_name = "home"
 
-urlpatterns = [path("", IndexView.as_view(), name="index"),
-               path("about", AboutView.as_view(), name="about")]
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+    path("about", AboutView.as_view(), name="about"),
+    path("profile", ProfileView.as_view(), name="profile"),
+]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,9 @@ from django.views.generic import TemplateView
 
 # import the comment model
 from confessions.models import Confession
+from accounts.models import User
+from comments.models import Course_Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class IndexView(TemplateView):
@@ -18,6 +21,27 @@ class IndexView(TemplateView):
         # bottom 5 confessions
         context["confessions"] = top_confessions
         return context
-    
+
+
 class AboutView(TemplateView):
     template_name = "about.html"
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        # get all the confessions from the database
+        user = User.objects.get(username=self.request.user)
+        comments = Course_Comment.objects.filter(user=user).order_by("-created_at")
+        total_upvotes = 0
+        total_downvotes = 0
+        for comment in comments:
+            total_upvotes += comment.upvotes_count
+            total_downvotes += comment.downvotes_count
+
+        context["total_upvotes"] = total_upvotes
+        context["total_downvotes"] = total_downvotes
+        context["comments"] = comments
+        return context
