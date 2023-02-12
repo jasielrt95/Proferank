c234be1d10d11a9630156a75da9c66edc17792a1
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-02-11 21:38:30 -0400

    adds prod and dev settings

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings")
+    if os.environ.get("ENVIRONMENT") == "PRODUCTION":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings.development")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
