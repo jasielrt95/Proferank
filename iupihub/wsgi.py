cc3f3f586256a9265f0135470d8708a4b93f81c5
Jasiel Rivera <44936597+jasielrt95@users.noreply.github.com>
2023-02-16 21:43:10 -0400

    try to fix server

--- iupihub/wsgi.py
+++ iupihub/wsgi.py
@@ -1,24 +1,11 @@
-"""
-WSGI config for iupihub project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = []
-path.append("/home/jasielrt95/iupihub")
-path.append("/home/jasielrt95/iupihub/iupihub")
-if path not in sys.path:
-    sys.path.append(path)
+path = ["/home/jasielrt95/iupihub", "/home/jasielrt95/iupihub/iupihub"]
+sys.path.extend(path)
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iupihub.settings.production")
 
 application = get_wsgi_application()
